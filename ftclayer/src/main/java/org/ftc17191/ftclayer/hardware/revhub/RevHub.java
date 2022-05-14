5b8bcc7b6ce4af78d8e54eccbabc2f7865888f91
Tate Wilhelm <t8wilhelm@gmail.com>
2022-05-13 19:54:39 -0500

    Fixed naming scheme of variables in RevHub

--- ftclayer/src/main/java/org/ftc17191/ftclayer/hardware/revhub/RevHub.java
+++ ftclayer/src/main/java/org/ftc17191/ftclayer/hardware/revhub/RevHub.java
@@ -17,32 +17,32 @@ import org.ftc17191.ftclayer.hardware.servos.servoex.ServoEx;
 public class RevHub {
     // Ids and hardware map
     public HardwareMap hardwareMap;
-    public String motor_ids[] = new String[4];
-    public String servo_ids[] = new String[6];
-    public String i2c_ids[] = new String[4];
+    public String motorIds[] = new String[4];
+    public String servoIds[] = new String[6];
+    public String i2CIds[] = new String[4];
     public MotorEx motor[] = new MotorEx[4];
     public ServoEx servo[] = new ServoEx[6];
 
     public RevHub(RevHubParameters parameters)
     {
         hardwareMap = parameters.hardwareMap;
-        motor_ids = parameters.motor;
-        servo_ids = parameters.servo;
-        i2c_ids = parameters.i2c;
+        motorIds = parameters.motor;
+        servoIds = parameters.servo;
+        i2CIds = parameters.i2c;
 
-        for (int i = 0; i < motor_ids.length; i++)
+        for (int i = 0; i < motorIds.length; i++)
         {
-            if (motor_ids[i] != null && motor_ids[i].length() != 0)
+            if (motorIds[i] != null && motorIds[i].length() != 0)
             {
-                motor[i] = new MotorEx(hardwareMap, motor_ids[i]);
+                motor[i] = new MotorEx(hardwareMap, motorIds[i]);
             }
         }
 
-        for (int i = 0; i < servo_ids.length; i++)
+        for (int i = 0; i < servoIds.length; i++)
         {
-            if (servo_ids[i] != null && servo_ids[i].length() != 0)
+            if (servoIds[i] != null && servoIds[i].length() != 0)
             {
-                servo[i] = new ServoEx(hardwareMap, servo_ids[i]);
+                servo[i] = new ServoEx(hardwareMap, servoIds[i]);
             }
         }
     }
